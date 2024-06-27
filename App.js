7f8689f1d6dfcdeb70b3fc7fd3cd238f1955a89c
vaishnaviitkare <vaishuitkare@gmail.com>
2024-06-27 18:43:43 +0530

    nested elements are created using react

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
+/*<div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+        <h2>I am h2 tag</h2>
+    </div>
+</div>
+*/
+//implement above nested element structure in react
 const heading =React.createElement("h1",
     //this object contain attributes
      {id:"heading"}, 
-     "Hello World from React");
+     React.createElement("div",{id:"child"},
+        [
+            //when there is more child content or element just add it in array
+            React.createElement("h1",{},"I am h1 tag"),
+            React.createElement("h2",{},"I am h2 tag")
+        ]
+     )
+    
+    );
      //Every element has props object that contain attributes and its content, store contain as its children
      //props={id:"heading",children:"Hello World from React"}
      //console.log(heading); -- object
